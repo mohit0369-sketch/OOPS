94d2354e0b97b9a1954d34d3b8659d9cb0e8e0a6
mohit0369-sketch <mohit111310@gmail.com>
2026-09-16 10:00:20 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> numbers = {10, 20, 30, 40, 50};
+    cout << "Elements of the collection: ";
+    for (auto element : numbers) {
+        cout << element << " ";
+    }
+    return 0;
+}
